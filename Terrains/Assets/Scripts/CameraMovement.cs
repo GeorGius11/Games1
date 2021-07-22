5bd65edacbcbc70fc0a5e5503512e0cad5a8f79b
George Rybko <george.rybko@gmail.com>
2021-07-22 23:49:07 +0300

    HomeWork3

---
+++ Terrains/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //[SerializeField] private float _sensivity = 10.0f;
+    [SerializeField] private GameObject _player;
+
+    private Vector3 _direction;
+
+    private void Update()
+    {
+        _direction = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        transform.Rotate((-1)*_direction.x, 0, 0);
+        //_player.transform.Rotate(0, _direction.y, 0);
+    }
+}
